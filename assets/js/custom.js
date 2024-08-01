77ffe7a088c6eb44e3f988ea8fb3cc9d437a6bc7
Grusha13 <shahgrusha@gmail.com>
2024-08-01 18:45:16 +0530

    new changes

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -80,8 +80,8 @@
         {
           breakpoint: 1024,
           settings: {
-            slidesToShow: 3,
-            slidesToScroll: 3,
+            slidesToShow: 2,
+            slidesToScroll: 1,
             infinite: true,
             dots: true
           }
